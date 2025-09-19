2a841b9e214455a9f9c3ceafb8e68e8251af2316
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-09-20 00:11:23 +0300

    Add validation to create sport

--- controllers/sport.controller.js
+++ controllers/sport.controller.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors');
 const Sport = require("../models/Sport");
 const CONSTANTS = require("../constants");
 
@@ -8,6 +9,6 @@ module.exports.createSport = async (req, res, next) => {
         const newSport = await Sport.create(body);
         res.status(201).send({ data: newSport });
     } catch (error) {
-        next(error);
+        next(createError(400, error.message));
     }
 };
--- routers/sport.router.js
+++ routers/sport.router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { createSport } = require('../controllers/sport.controller');
 const upload = require('../middlewares/uploadImage');
+const { validateSportBody } = require('../middlewares/sport.mw');
+const { sportSchemaCreate } = require('../validations/sport.validation');
 
 const sportRouter = express.Router();
 
-sportRouter.post('/', upload.single('image'), createSport);
+sportRouter.post('/', validateSportBody(sportSchemaCreate), upload.single('image'), createSport);
 
 module.exports = sportRouter;
