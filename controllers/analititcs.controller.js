20fdb5149951248bc827afc1babdbea5b58f48bb
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-09-24 21:27:07 +0300

    Add countAthletesByCountry, countSportsByCountry, avgAgeAthletesBySport

--- controllers/analititcs.controller.js
+++ controllers/analititcs.controller.js
@@ -2,6 +2,125 @@ const createError = require('http-errors');
 const Sport = require('../models/Sport');
 const Athlete = require('../models/Athlete');
 
+module.exports.countSportsByCountry = async (req, res, next) => {
+    try {
+        const data = await Sport.aggregate([
+            {
+                $lookup: {
+                    from: 'athletes',
+                    localField: '_id',
+                    foreignField: 'sportId',
+                    as: 'athletes',
+                },
+            },
+            {
+                $match: {
+                    'athletes.0': { $exists: true },
+                },
+            },
+            {
+                $unwind: '$athletes',
+            },
+            {
+                $group: {
+                    _id: { country: '$athletes.country', sport: '$name' },
+                },
+            },
+            {
+                $group: {
+                    _id: '$_id.country',
+                    amount: { $sum: 1 },
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                    country: '$_id',
+                    amount: 1,
+                },
+            },
+            {
+                $sort: {
+                    amount: -1,
+                },
+            },
+        ]);
+        res.status(200).send({ data });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.averageAgeAthletesBySport = async (req, res, next) => {
+    try {
+        const currentYear = new Date().getFullYear();
+        const data = await Athlete.aggregate([
+            {
+                $group: {
+                    _id: '$sportId',
+                    avgAge: {
+                        $avg: { $subtract: [currentYear, '$birthYear'] },
+                    },
+                },
+            },
+            {
+                $lookup: {
+                    from: 'sports',
+                    localField: '_id',
+                    foreignField: '_id',
+                    as: 'sport',
+                },
+            },
+            {
+                $unwind: '$sport',
+            },
+            {
+                $project: {
+                    _id: 0,
+                    sport: '$sport.name',
+                    avgAge: { $round: ['$avgAge', 1] },
+                },
+            },
+            {
+                $sort: {
+                    avgAge: 1,
+                },
+            },
+        ]);
+        res.status(200).send({ data });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.countAthletesByCountry = async (req, res, next) => {
+    try {
+        const data = await Athlete.aggregate([
+            {
+                $group: {
+                    _id: '$country',
+                    amount: { $sum: 1 },
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                    country: '$_id',
+                    amount: 1,
+                },
+            },
+            {
+                $sort: {
+                    amount: -1,
+                },
+            },
+        ]);
+        res.status(200).send({ data });
+    } catch (error) {
+        next(error);
+    }
+};
+
 module.exports.countAthletesBySport = async (req, res, next) => {
     try {
         const data = await Sport.aggregate([
@@ -10,27 +129,27 @@ module.exports.countAthletesBySport = async (req, res, next) => {
                     from: 'athletes',
                     localField: '_id',
                     foreignField: 'sportId',
-                    as: 'athletes'
-                }
+                    as: 'athletes',
+                },
             },
             {
                 $project: {
                     _id: 0,
                     sport: '$name',
                     count: { $size: '$athletes' },
-                }
+                },
             },
             {
                 $sort: {
-                    count: 1
-                }
+                    count: 1,
+                },
             },
             {
-                $limit: 3
-            }
+                $limit: 3,
+            },
         ]);
         res.status(200).send({ data });
     } catch (error) {
-        next(createError(400, error.message));
+        next(error);
     }
 };
--- routers/analitics.router.js
+++ routers/analitics.router.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { countAthletesBySport } = require('../controllers/analititcs.controller');
+const { countAthletesBySport, countAthletesByCountry, averageAgeAthletesBySport, countSportsByCountry } = require('../controllers/analititcs.controller');
 
 const analiticsRouter = express.Router();
 
 analiticsRouter.get('/amount-athletes-by-sport', countAthletesBySport);
+analiticsRouter.get('/amount-athletes-by-country', countAthletesByCountry);
+analiticsRouter.get('/average-age-athletes-by-sport', averageAgeAthletesBySport);
+analiticsRouter.get('/amount-sports-by-country', countSportsByCountry);
 
 module.exports = analiticsRouter;
